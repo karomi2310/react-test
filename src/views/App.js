27e47d94bc3b9db5c4816746bd8b4b9c52c591e6
Huy Hoang <hoangnguyen23102000@gmail.com>
2021-11-17 09:30:41 +0700

    init project

---
+++ src/views/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const sum = (a, b) => (a + b)
+
+
+  console.log(sum(2, 2))
+  return (
+    <div></div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+export default App;
